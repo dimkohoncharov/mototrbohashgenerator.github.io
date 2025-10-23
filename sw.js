c38023474f5a022ffe9ef13f84fb46788ec0bc68
dimkohoncharov <honcharov.dimko@gmail.com>
2025-10-23 22:39:57 +0300

    add splashscreen

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
-const CACHE = 'm-key-cache-v1';
-const ASSETS = [
-  '/index.html',
-  '/app.js',
-  '/manifest.webmanifest'
-];
-self.addEventListener('install', (e) => {
+// sw.js
+const CACHE = 'm-key-cache-v3';
+const ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/app.js'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting(); // одразу активуємо новий SW (за потреби)
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-self.addEventListener('activate', (e) => {
+
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
   );
+  self.clients.claim(); // керуємо відкритими вкладками
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
